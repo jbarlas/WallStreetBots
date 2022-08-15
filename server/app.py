cff7885b659fecd750de587eb34f80f7752e28f9
jbarlas <77653620+jbarlas@users.noreply.github.com>
2022-08-14 23:45:11 -0400

    allow cors

--- server/app.py
+++ server/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from routes.submissions import add_submissions_by_day, submission_history, recent_submission
 from PRAW.praw_utils import comment_stream
 
+
+
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
@@ -11,14 +15,14 @@ def test():
 
 @app.route('/add-submissions/<day-utc>')
 def submissions_by_day(day_utc):
-    # """
-    # Gather top submissions for 24hr after given utc in unix format
-    #     Returns: 
-    #         {
-    #             data: Submission[],
-    #             next_utc: number # utc for following day
-    #         }
-    # """
+    """
+    Gather top submissions for 24hr after given utc in unix format
+        Returns: 
+            {
+                data: Submission[],
+                next_utc: number # utc for following day
+            }
+    """
     return add_submissions_by_day(day_utc), 200
 
 @app.route('/recent-submission')
